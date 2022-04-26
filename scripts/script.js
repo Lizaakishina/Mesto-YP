6ce42f46f4d8c14e62c97adb45e0412b6f29845e
Akishina Elizaveta <lizaakishina@gmail.com>
2022-04-26 13:15:17 +0300

    na rev 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,29 @@
 let popup = document.querySelector('.popup');
-let editbutton = document.querySelector('.profile__edit');
-let closebutton = document.querySelector('.popup__close');
+let editButton = document.querySelector('.profile__edit');
+let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup-form');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+let inputName = document.querySelector('.popup-fieldset__input_type_name');
+let inputJob = document.querySelector('.popup-fieldset__input_type_job');
+
 function openpopup(){
     popup.classList.add('popup_opened');
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
 }
+
 function closepopup(){
    popup.classList.remove('popup_opened');
 }
-let formElement = document.querySelector('.popup-form');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
-let inputName = document.querySelector('.popup-fieldset__input_name');
-let inputJob = document.querySelector('.popup-fieldset__input_job');
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
     closepopup();
 }
-editbutton.addEventListener('click', openpopup);
-closebutton.addEventListener('click', closepopup);
+
+editButton.addEventListener('click', openpopup);
+closeButton.addEventListener('click', closepopup);
 formElement.addEventListener('submit', formSubmitHandler);
