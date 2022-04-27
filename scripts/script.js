d8427f29eda54055387801439aff4f3a59b7d0bb
Akishina Elizaveta <lizaakishina@gmail.com>
2022-04-27 10:26:01 +0300

    na rev 3

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ let closeButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup-form');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
-let inputName = document.querySelector('.popup-fieldset__input_type_name');
-let inputJob = document.querySelector('.popup-fieldset__input_type_job');
+let inputName = document.querySelector('.popup-form__fieldset-input_type_name');
+let inputJob = document.querySelector('.popup-form__fieldset-input_type_job');
 
 function openpopup(){
     popup.classList.add('popup_opened');
